93a796376e3bd2034d968e7608974a6775b8ae2f
Konstantin Läufer <klaeufer@gmail.com>
2020-11-19 16:41:15 -0600

    separated routes and actions
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- sparkbadge.py
+++ sparkbadge.py
@@ -1,39 +1,14 @@
 #!/usr/bin/env python3
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, make_response
+from actions import sparkline_random
 
-from flask import redirect, make_response
-from quickchart import QuickChart
-import requests
-import random
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello and welcome to SparkBadge!"
+    return 'Hello and welcome to SparkBadge! <a href="/random">Try this.</a>'
 
 @app.route('/random')
-def sparkline_random():
-
-    # generate image for badge
-    qc = QuickChart()
-    qc.width = 75
-    qc.height = 25
-    #qc.device_pixel_ratio = 2.0
-    qc.config = {
-        "type": "sparkline",
-        "data": {
-            "datasets": [{
-                "data": random.sample(range(10, 30), 10)
-            }]
-        }
-    }
-    badge = requests.get(qc.get_short_url()).content
-
-    # serve image with suitable cache control headers
-    res = make_response(badge)
-    res.headers.set('Content-Type', 'image/png')
-    res.headers.set('Cache-Control', 'no-cache, no-store, must-revalidate, max-age=0')
-    res.headers.set('Pragma', 'no-cache')
-    res.headers.set('Expires', '0')
-    return res
+def random():
+    return sparkline_random()
