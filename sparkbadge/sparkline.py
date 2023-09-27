ed6c65dea827ff88316e2dfa871884751d27d4ef
Shane Panchot <shane.panchot@gmail.com>
2023-09-27 18:47:51 -0500

    updated to drawsvg version 2.0

--- sparkbadge/sparkline.py
+++ sparkbadge/sparkline.py
@@ -56,7 +56,7 @@ def hist_trend(samples: List[int], stroke_color: str, stroke_width: int) -> str:
                   fill='none'))
 
     #return canvas.asDataUri()
-    return canvas.asSvg()
+    return canvas.as_svg()
 
 
 def trend(samples: List[int], stroke_color: str, stroke_width: int) -> str or None:
@@ -75,7 +75,7 @@ def trend(samples: List[int], stroke_color: str, stroke_width: int) -> str or No
     canvas.append(path)
     
     #return canvas.asDataUri()
-    return canvas.asSvg()
+    return canvas.as_svg()
 
 
 def stacked_bar(samples, labels):
@@ -99,7 +99,7 @@ def stacked_bar(samples, labels):
         #canvas.append(draw.Rectangle(10*i, yy1, 10, yy2, fill='purple'))
     '''
 
-    canvas.saveSvg('/home/nick/dl/sbar.svg')
+    canvas.save_svg('../examples')
     #return canvas.asSvg()
 
 
